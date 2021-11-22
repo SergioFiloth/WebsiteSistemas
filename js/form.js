b71361c490da2477548c15f57e019f952ffa57cb
Sergio Filoth <sergiofiloth@gmail.com>
2021-11-21 23:41:15 -0500

    Finalizacion pagina de quien soy

---
+++ js/form.js
@@ -0,0 +1,6 @@
+let form = document.querySelector(".Form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  alert("Esta opcion no esta habilitada, intente mas tarde.");
+});
--- who/index.php
+++ who/index.php
@@ -3,11 +3,9 @@
 
 <head>
     <link rel="stylesheet" href="../css/styles.css">
-
     <?php
         include "../templates/head.php"
     ?>
-
     <title>Mi hoja de vida - Quien Soy</title>
 </head>
 
@@ -19,9 +17,67 @@
             </li>
         </ul>
     </main>
+    <div class="Info">
+        <h2 class="Info__title">
+            ¿Quien Soy?
+        </h2>
+        <div class="Info--container">
+            <img src="../img/personal-info.svg" alt="Personal Info svg" class="Info__img">
+            <p class="Info__description">
+                Soy Sergio Filoth Valiente, estudiante del grado 11° en el C.E.A Puerto Tejada, tengo 17 años de edad actualmente y pienso estudiar Ingenieria en Sistemas.
+            </p>
+        </div>
+    </div>
+    <form class="Form">
+        <div class="Form-content">
+            <p class="Form__title">
+                Contáctame
+            </p>
+            <div class="Form-content--name">
+                <label for="name">Tu nombre completo<span>*</span></label>
+                <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    placeholder="Nombre"
+                >
+            </div>
+            <div class="Form-content--email">
+                <label for="email">Tu email <span>*</span></label>
+                <input
+                    type="text"
+                    name="email"
+                    id="email"
+                    placeholder="tu@email.com"
+                >
+            </div>
+            <div class="Form-content--message">
+                <label for="about" class="Form-content--label__message">Cuéntame de tu proyecto <span>*</span></label>
+                <div class="Form__textarea">
+                    <textarea
+                        name="message"
+                        id="about"
+                        class="textarea"
+                        placeholder="Hablame de tu proyecto, minimo 75 caracteres"
+                    ></textarea>
+                </div>
+            </div>
+            <input
+                type="submit"
+                value="Pide tu cotización"
+                class="btn"
+            >
+        </div>
+    </form>
     <?php
         include "../templates/Footer.php"
     ?>
+    <script src="../js/form.js"></script>
 </body>
+<style>
+    .Info__title{
+        margin-bottom: 2rem;
+    }
+</style>
 
 </html>
